0d07d6e5c6a43fc1296241ac11db5a7fd93748bc
Lauren Ball <43153772+locoball@users.noreply.github.com>
2019-05-24 22:02:11 -0400

    Delete PS1.P1.js

--- PS1.P1.js
+++
@@ -1,6 +0,0 @@
-//Problem Set 1
-//Problem 1
-
-//Sorts an input string by alphabetical order
-const alphabetical = jumbled => ([...jumbled].sort()).join('');
-console.log(alphabetical('supercalifragilisticexpialidocious'));
